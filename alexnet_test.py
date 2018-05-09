fd5f07102947c5dc4b4406f094765deee2269a48
Youngkyung <syg0413@gmail.com>
2018-05-09 14:40:02 +0900

    edit

--- alexnet_test.py
+++ alexnet_test.py
@@ -1,205 +1,146 @@
-"""Script to finetune AlexNet using Tensorflow.
-With this script you can finetune AlexNet as provided in the alexnet.py
-class on any given dataset. Specify the configuration settings at the
-beginning according to your problem.
-This script was written for TensorFlow >= version 1.2rc0 and comes with a blog
-post, which you can find here:
-https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
-Author: Frederik Kratzert
-contact: f.kratzert(at)gmail.com
-"""
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for slim.nets.alexnet."""
 
-import os
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-import numpy as np
-import tensorflow as tf
-
-from alexnet import AlexNet
-from datagenerator import ImageDataGenerator
-from datetime import datetime
-from tensorflow.contrib.data import Iterator
-
-"""
-Configuration Part.
-"""
-
-# Path to the textfiles for the trainings and validation set
-train_file = '/path/to/train.txt'
-val_file = '/path/to/val.txt'
-
-# Learning params
-learning_rate = 0.01
-num_epochs = 10
-batch_size = 128
-
-# Network params
-dropout_rate = 0.5
-num_classes = 2
-train_layers = ['fc8', 'fc7', 'fc6']
-
-# How often we want to write the tf.summary data to disk
-display_step = 20
-
-# Path for tf.summary.FileWriter and to store model checkpoints
-filewriter_path = "/tmp/finetune_alexnet/tensorboard"
-checkpoint_path = "/tmp/finetune_alexnet/checkpoints"
-
-"""
-Main Part of the finetuning Script.
-"""
-
-# Create parent path if it doesn't exist
-if not os.path.isdir(checkpoint_path):
-    os.mkdir(checkpoint_path)
-
-# Place data loading and preprocessing on the cpu
-with tf.device('/cpu:0'):
-    tr_data = ImageDataGenerator(train_file,
-                                 mode='training',
-                                 batch_size=batch_size,
-                                 num_classes=num_classes,
-                                 shuffle=True)
-    val_data = ImageDataGenerator(val_file,
-                                  mode='inference',
-                                  batch_size=batch_size,
-                                  num_classes=num_classes,
-                                  shuffle=False)
-
-    # create an reinitializable iterator given the dataset structure
-    iterator = Iterator.from_structure(tr_data.data.output_types,
-                                       tr_data.data.output_shapes)
-    next_batch = iterator.get_next()
-
-# Ops for initializing the two different iterators
-training_init_op = iterator.make_initializer(tr_data.data)
-validation_init_op = iterator.make_initializer(val_data.data)
-
-# TF placeholder for graph input and output
-x = tf.placeholder(tf.float32, [batch_size, 227, 227, 3])
-y = tf.placeholder(tf.float32, [batch_size, num_classes])
-keep_prob = tf.placeholder(tf.float32)
-
-# Initialize model
-model = AlexNet(x, keep_prob, num_classes, train_layers)
-
-# Link variable to model output
-score = model.fc8
-
-# List of trainable variables of the layers we want to train
-var_list = [v for v in tf.trainable_variables() if v.name.split('/')[0] in train_layers]
-
-# Op for calculating the loss
-with tf.name_scope("cross_ent"):
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=score,
-                                                                  labels=y))
-
-# Train op
-with tf.name_scope("train"):
-    # Get gradients of all trainable variables
-    gradients = tf.gradients(loss, var_list)
-    gradients = list(zip(gradients, var_list))
-
-    # Create optimizer and apply gradient descent to the trainable variables
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.apply_gradients(grads_and_vars=gradients)
-
-# Add gradients to summary
-for gradient, var in gradients:
-    tf.summary.histogram(var.name + '/gradient', gradient)
-
-# Add the variables we train to the summary
-for var in var_list:
-    tf.summary.histogram(var.name, var)
-
-# Add the loss to summary
-tf.summary.scalar('cross_entropy', loss)
+from tensorflow.contrib.framework.python.ops import variables as variables_lib
+from tensorflow.contrib.slim.python.slim.nets import alexnet
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import random_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import variables
+from tensorflow.python.platform import test
 
 
-# Evaluation op: Accuracy of the model
-with tf.name_scope("accuracy"):
-    correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+class AlexnetV2Test(test.TestCase):
 
-# Add the accuracy to the summary
-tf.summary.scalar('accuracy', accuracy)
+  def testBuild(self):
+    batch_size = 5
+    height, width = 224, 224
+    num_classes = 1000
+    with self.test_session():
+      inputs = random_ops.random_uniform((batch_size, height, width, 3))
+      logits, _ = alexnet.alexnet_v2(inputs, num_classes)
+      self.assertEquals(logits.op.name, 'alexnet_v2/fc8/squeezed')
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
 
-# Merge all summaries together
-merged_summary = tf.summary.merge_all()
+  def testFullyConvolutional(self):
+    batch_size = 1
+    height, width = 300, 400
+    num_classes = 1000
+    with self.test_session():
+      inputs = random_ops.random_uniform((batch_size, height, width, 3))
+      logits, _ = alexnet.alexnet_v2(inputs, num_classes, spatial_squeeze=False)
+      self.assertEquals(logits.op.name, 'alexnet_v2/fc8/BiasAdd')
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, 4, 7, num_classes])
 
-# Initialize the FileWriter
-writer = tf.summary.FileWriter(filewriter_path)
+  def testEndPoints(self):
+    batch_size = 5
+    height, width = 224, 224
+    num_classes = 1000
+    with self.test_session():
+      inputs = random_ops.random_uniform((batch_size, height, width, 3))
+      _, end_points = alexnet.alexnet_v2(inputs, num_classes)
+      expected_names = [
+          'alexnet_v2/conv1', 'alexnet_v2/pool1', 'alexnet_v2/conv2',
+          'alexnet_v2/pool2', 'alexnet_v2/conv3', 'alexnet_v2/conv4',
+          'alexnet_v2/conv5', 'alexnet_v2/pool5', 'alexnet_v2/fc6',
+          'alexnet_v2/fc7', 'alexnet_v2/fc8'
+      ]
+      self.assertSetEqual(set(end_points.keys()), set(expected_names))
 
-# Initialize an saver for store model checkpoints
-saver = tf.train.Saver()
+  def testModelVariables(self):
+    batch_size = 5
+    height, width = 224, 224
+    num_classes = 2
+    with self.test_session():
+      inputs = random_ops.random_uniform((batch_size, height, width, 3))
 
-# Get the number of training/validation steps per epoch
-train_batches_per_epoch = int(np.floor(tr_data.data_size/batch_size))
-val_batches_per_epoch = int(np.floor(val_data.data_size / batch_size))
 
-# Start Tensorflow session
-with tf.Session() as sess:
+      alexnet.alexnet_v2(inputs, num_classes)
+      expected_names = [
+          'alexnet_v2/conv1/weights',
+          'alexnet_v2/conv1/biases',
+          'alexnet_v2/conv2/weights',
+          'alexnet_v2/conv2/biases',
+          'alexnet_v2/conv3/weights',
+          'alexnet_v2/conv3/biases',
+          'alexnet_v2/conv4/weights',
+          'alexnet_v2/conv4/biases',
+          'alexnet_v2/conv5/weights',
+          'alexnet_v2/conv5/biases',
+          'alexnet_v2/fc6/weights',
+          'alexnet_v2/fc6/biases',
+          'alexnet_v2/fc7/weights',
+          'alexnet_v2/fc7/biases',
+          'alexnet_v2/fc8/weights',
+          'alexnet_v2/fc8/biases',
+      ]
+      model_variables = [v.op.name for v in variables_lib.get_model_variables()]
+      self.assertSetEqual(set(model_variables), set(expected_names))
 
-    # Initialize all variables
-    sess.run(tf.global_variables_initializer())
+  def testEvaluation(self):
+    batch_size = 2
+    height, width = 224, 224
+    num_classes = 1000
+    with self.test_session():
+      eval_inputs = random_ops.random_uniform((batch_size, height, width, 3))
+      logits, _ = alexnet.alexnet_v2(eval_inputs, is_training=False)
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
+      predictions = math_ops.argmax(logits, 1)
+      self.assertListEqual(predictions.get_shape().as_list(), [batch_size])
 
-    # Add the model graph to TensorBoard
-    writer.add_graph(sess.graph)
+  def testTrainEvalWithReuse(self):
+    train_batch_size = 2
+    eval_batch_size = 1
+    train_height, train_width = 224, 224
+    eval_height, eval_width = 300, 400
+    num_classes = 1000
+    with self.test_session():
+      train_inputs = random_ops.random_uniform(
+          (train_batch_size, train_height, train_width, 3))
+      logits, _ = alexnet.alexnet_v2(train_inputs)
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [train_batch_size, num_classes])
+      variable_scope.get_variable_scope().reuse_variables()
+      eval_inputs = random_ops.random_uniform(
+          (eval_batch_size, eval_height, eval_width, 3))
+      logits, _ = alexnet.alexnet_v2(
+          eval_inputs, is_training=False, spatial_squeeze=False)
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [eval_batch_size, 4, 7, num_classes])
+      logits = math_ops.reduce_mean(logits, [1, 2])
+      predictions = math_ops.argmax(logits, 1)
+      self.assertEquals(predictions.get_shape().as_list(), [eval_batch_size])
 
-    # Load the pretrained weights into the non-trainable layer
-    model.load_initial_weights(sess)
+  def testForward(self):
+    batch_size = 1
+    height, width = 224, 224
+    with self.test_session() as sess:
+      inputs = random_ops.random_uniform((batch_size, height, width, 3))
+      logits, _ = alexnet.alexnet_v2(inputs)
+      sess.run(variables.global_variables_initializer())
+      output = sess.run(logits)
+      self.assertTrue(output.any())
 
-    print("{} Start training...".format(datetime.now()))
-    print("{} Open Tensorboard at --logdir {}".format(datetime.now(),
-                                                      filewriter_path))
 
-    # Loop over number of epochs
-    for epoch in range(num_epochs):
-
-        print("{} Epoch number: {}".format(datetime.now(), epoch+1))
-
-        # Initialize iterator with the training dataset
-        sess.run(training_init_op)
-
-        for step in range(train_batches_per_epoch):
-
-            # get next batch of data
-            img_batch, label_batch = sess.run(next_batch)
-
-            # And run the training op
-            sess.run(train_op, feed_dict={x: img_batch,
-                                          y: label_batch,
-                                          keep_prob: dropout_rate})
-
-            # Generate summary with the current batch of data and write to file
-            if step % display_step == 0:
-                s = sess.run(merged_summary, feed_dict={x: img_batch,
-                                                        y: label_batch,
-                                                        keep_prob: 1.})
-
-                writer.add_summary(s, epoch*train_batches_per_epoch + step)
-
-        # Validate the model on the entire validation set
-        print("{} Start validation".format(datetime.now()))
-        sess.run(validation_init_op)
-        test_acc = 0.
-        test_count = 0
-        for _ in range(val_batches_per_epoch):
-
-            img_batch, label_batch = sess.run(next_batch)
-            acc = sess.run(accuracy, feed_dict={x: img_batch,
-                                                y: label_batch,
-                                                keep_prob: 1.})
-            test_acc += acc
-            test_count += 1
-        test_acc /= test_count
-        print("{} Validation Accuracy = {:.4f}".format(datetime.now(),
-                                                       test_acc))
-        print("{} Saving checkpoint of model...".format(datetime.now()))
-
-        # save checkpoint of the model
-        checkpoint_name = os.path.join(checkpoint_path,
-                                       'model_epoch'+str(epoch+1)+'.ckpt')
-        save_path = saver.save(sess, checkpoint_name)
-
-        print("{} Model checkpoint saved at {}".format(datetime.now(),
-                                                       checkpoint_name))
+if __name__ == '__main__':
+  test.main()
